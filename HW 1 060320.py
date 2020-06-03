283f999c754d85b70eb796cb05d6551c69bf948f
jbjuniper <66389941+jbjuniper@users.noreply.github.com>
2020-06-03 19:02:19 -0400

    Update HW 1 060320.py
    slight updates

--- HW 1 060320.py
+++ HW 1 060320.py
@@ -2,14 +2,9 @@
 
 import math
 
-
 # homework # 1 060320
 for x in range(1,3):
     print("---")
-
-
-
-
 
 print("PROBLEM #1 - implement power and **")
 x=int(input("input number to be raised by the power "))
@@ -20,18 +15,14 @@ print("   using math.pow ",x," to the power of =",y," = ",math.pow(x,y))
 for x in range(1,3):
     print("---")
 
-y=11
 print("PROBLEM #2 - implement mod and %  using x%y")
 x=int(input("input nunber to be divided "))
 y=int(input("input number to divide by "))
-
-
 flip=math.floor(x/y)*y
 print(x," /",y," = ",x/y," so floor(",x,"%",y,")*",y," is ",flip," and ",x,"-",flip,"=",x-flip," therefore y%x (the remainder) is ",x%y)
 
 for x in range(1,3):
     print("---")
-
 
 print("PROBLEM #3 - implement any other math function - I elected perm")
 
@@ -44,10 +35,3 @@ while not int(k) in range(1,11) :
     k=int(input("Define k (items selected each time) max of 10 - "))
 
 print("permutations of ",k," items selected from ",n,"items is ",format(math.perm(n,k),",d"))
-
-
-
-
-
-# create a GitHub account
-
